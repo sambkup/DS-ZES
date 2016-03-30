0296aa07132ea1141d33468c62f3fc1292eedc30
Sammy <skupfer@andrew.cmu.edu>
2016-03-29 20:45:44 -0400

    merged refactoring

--- src/process/TestBench.java
+++ src/process/TestBench.java
@@ -1,15 +1,13 @@
 package process;
 
-import java.io.IOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
+
 import javax.swing.JOptionPane;
 
 import communication.MessagePasser;
 import services.ClockService;
-import utils.P2P;
+import utils.Node;
 
 public class TestBench {
 
@@ -33,7 +31,7 @@ public class TestBench {
 	 * - serially ping server/ports
 	 * - maybe a small timeout?
 	 * - Just need to find one, and that will help you get to the next one
-	 * - 
+	 * - For demo, serially check every port in localhost
 	 * 
 	 * 
 	 */
@@ -41,7 +39,6 @@ public class TestBench {
 	
 	//private static String name;
 
-	private static P2P p2p_network;
 	private static String name;
 	private static int port;
 	private static MessagePasser messagePasser;
@@ -60,21 +57,15 @@ public class TestBench {
 		// construct the required objects
 
 		clock = ClockService.clockServiceFactory("logical");
-		p2p_network = new P2P(name,80.9000,-90.000,port);
+		Node myself = new Node(name,80.9000,-90.000,port, findMyIPaddr());
 
-		messagePasser = new MessagePasser(p2p_network,clock);
+		messagePasser = new MessagePasser(myself, clock);
 		
-		System.out.println(p2p_network.getMyNode().toString());
+		System.out.println(messagePasser.localNode.toString());
 		
-		System.out.println(findFirstNode());
 		
 		// --------------------------------
 		// Execute something here
-		
-		
-		
-		
-
 		
 		
 		//prompt();
@@ -82,49 +73,22 @@ public class TestBench {
 		
 	}
 	
-	public static boolean findFirstNode(){
-		String myIP = "10.0.0.183";
-		String delims = "[.]";
-		String[] chunks = myIP.split(delims);
-		int maxIP = 256;
-		String testIP;
-		for (int i = 1; i<maxIP; i++){
-			if (Integer.toString(i).equals(chunks[3])){
-				continue;
-			}
-
-			testIP = chunks[0]+"."+chunks[1]+"."+chunks[2]+"."+i;
-			Socket s = null;
-			try {
-				//InetAddress inetAddress = InetAddress.getByName(testIP);
-				InetSocketAddress endpoint = new InetSocketAddress(testIP, port);
-
-				s = new Socket();
-				s.connect(endpoint, 100);
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println(testIP + " - Closed");
-				continue;
-
-				//e.printStackTrace();
-			}finally{
-				try {
-					s.close();
-				} catch (IOException e) {
-				}
-			}
-			System.out.println(testIP + " - Open");
-			return true;
-		}
-		
-		return false;
-	}
-
+	
 	
 
+	private static String findMyIPaddr(){
+		// TODO: make this failure tolerant
+		InetAddress x = null;
+		try {
+			x = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return x.getHostAddress();
+	}
+
+
+	@SuppressWarnings("unused")
 	private static void prompt() {
 		Thread receiver_thread = new Thread() {
 			public void run() {
--- src/utils/P2P.java
+++ src/utils/P2P.java
@@ -1,6 +1,8 @@
 package utils;
 
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,13 +12,17 @@ public class P2P {
 	String processName;
 	List<Node> foundNodes;
 	Node myNode;
+	int nodecount = 0;
 
 	Object config_data;
 
-	public P2P(String name,double lattitude, double longitude, int port) {
-		this.processName = name;
+	public P2P(Node myself) {
+		this.myNode = myself;
+
+		this.processName = myNode.name;
 		this.foundNodes = new ArrayList<Node>();
-		this.myNode = new Node(name,lattitude, longitude, port,findIPaddr());
+		System.out.println("Starting bootstrapping...");
+		findFirstNode();
 		// TODO: call bootstrap() and block until at least one node is found.
 	}
 
@@ -33,11 +39,59 @@ public class P2P {
 		}
 		return null;
 	}
-	
-	public synchronized boolean findFirstNode(){
-		
-		
+
+	public boolean findFirstNode(){
+		String myIP = this.myNode.ip;
+		String delims = "[.]";
+		String[] chunks = myIP.split(delims);
+
+		int maxIP = 256;
+		String testIP;
+		for (int i = 1; i<maxIP; i++){
+			if (Integer.toString(i).equals(chunks[3])){
+				continue;
+			}
+
+			testIP = chunks[0]+"."+chunks[1]+"."+chunks[2]+"."+i;
+			Socket s = null;
+			try {
+				InetSocketAddress endpoint = new InetSocketAddress(testIP, this.myNode.port);
+				s = new Socket();
+				s.connect(endpoint, 100);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				//System.out.println(testIP + " - Closed");
+				continue;
+			}
+			if (s != null){
+				try {
+					s.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			System.out.println(testIP + " - Found first node");
+			addFoundNode(testIP);
+			return true;
+		}
 		return false;
+	}
+	
+	private void addFoundNode(String IP){
+		String name = "Node"+this.nodecount++;
+		// TODO: Request details from this node - latt, long
+
+		double lattitude = 0.0;
+		double longitude = 0.0;
+		int port = this.myNode.port;
+		Node newNode = new Node(name,lattitude, longitude, port,IP);
+		synchronized (foundNodes){
+			foundNodes.add(newNode);
+		}
+		
 	}
 
 	public synchronized Node getNode(String name) {
@@ -68,17 +122,6 @@ public class P2P {
 
 		return -1;
 	}
-	
-	private static String findIPaddr(){
-		// TODO: make this failure tolerant
-		InetAddress x = null;
-		try {
-			x = InetAddress.getLocalHost();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		return x.getHostAddress();
-	}
 
 
 	/* Setters and Getters */
@@ -96,7 +139,7 @@ public class P2P {
 		}
 		return nodesList;
 	}
-	
+
 	public Node getMyNode(){
 		return myNode;
 	}
