69215a233f4f96d63a0b80106b8add70a2304674
Sammy Kupfer <sammy@enertiv.com>
2016-04-18 23:33:38 -0400

    terrible workflow !!!

--- src/utils/NodePatrolArea.java
+++ src/utils/NodePatrolArea.java
@@ -51,15 +51,15 @@ public class NodePatrolArea implements Serializable{
 		System.out.printf("(%f,%f);(%f,%f);(%f,%f);(%f,%f)\n", a1,b1,a2,b2,a3,b3,a4,b4);
 		if (a2==a3 || a1==a4){
 			System.out.println("1");
-			if ( (a4-a3) <= (a2-a1)){
+			if ( (b4-b3) <= (b2-b1)){
 				System.out.println("2");
-				if ( ( a3>=a2) && (a3<=a1) ){
+				if ( ( b3>=b2) && (b3<=b1) ){
 					System.out.println("3");
 					return true;
 				}
 			} else {
 				System.out.println("4");
-				if ( ( a1<=a3) && (a1>=a4) ){
+				if ( ( b1<=b3) && (b1>=b4) ){
 					System.out.println("5");
 					return true;
 				}
@@ -67,15 +67,15 @@ public class NodePatrolArea implements Serializable{
 			
 		} else if (b1==b4 || b2==b3){
 			System.out.println("6");
-			if ( (b2-b1) >= (b4-b3) ){
+			if ( (a2-a1) >= (a4-a3) ){
 				System.out.println("7");
-				if ( (b3 <= b2) && (b3 >= b1) ){
+				if ( (a3 <= a2) && (a3 >= a1) ){
 					System.out.println("8");
 					return true;
 				}
 			} else{
 				System.out.println("9");
-				if ( (b2 >= b3) && ( b2 <= b4) ){
+				if ( (a2 >= a3) && ( a2 <= a4) ){
 					System.out.println("10");
 					return true;
 				}
