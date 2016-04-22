c0d90a9675cef93b3936cfab32a600571e404682
Sammy <skupfer@andrew.cmu.edu>
2016-04-21 20:56:39 -0400

    mods

--- src/utils/Node.java
+++ src/utils/Node.java
@@ -74,7 +74,7 @@ public class Node implements Serializable {
 		 * 3. decide which region belongs to which node
 		 */
 		
-		System.out.println("|x2-x1|="+Math.abs(x2-x1)+" |y2-y1|="+Math.abs(y2-y1));
+		System.out.println("|x2-x1|="+Math.abs(x2-x1)+" |y2-y1|="+Math.abs(y2-y1)+" |x2-x1|==|y2-y1|="+(Math.abs(x2-x1) == Math.abs(y2-y1)));
 		
 		if (Math.abs(x2-x1) == Math.abs(y2-y1)){
 			// square - now figure out which axis to split along
